ec129315bddbdf7a20f5b52749d208d5c6c3156a
sathvikavaddepally <sathvikavaddepally@gmail.com>
2025-12-12 09:22:29 +0530

    basic try catch

---
+++ trycatch.java
@@ -0,0 +1,18 @@
+public class trycatch {
+    public static void main(String[]args){
+        try{
+            int a=10;
+            int b=0;
+            int result = a/b;
+            System.out.println("Result:" + result);
+        }  catch(Exception s){
+            System.out.println("caught Exeption :"+ s.getMessage());
+
+        }
+        
+       
+    }
+        
+    }
+    
+
