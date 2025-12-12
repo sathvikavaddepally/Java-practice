ea484d26cac11ae6485b7fb8d660573d250aa9ac
sathvikavaddepally <sathvikavaddepally@gmail.com>
2025-12-12 15:13:23 +0530

    number palindrome

---
+++ numberpalindrome.java
@@ -0,0 +1,19 @@
+public class numberpalindrome {
+    public static void main(String[] args) {
+        int num = 121;   // test number
+        int original = num;
+        int reversed = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num = num / 10;
+        }
+
+        if (original == reversed) {
+            System.out.println(original + " is a palindrome.");
+        } else {
+            System.out.println(original + " is not a palindrome.");
+        }
+    }
+}
