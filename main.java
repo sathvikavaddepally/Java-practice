8eb683aac08b9e91aa04de980b70b883b8cf5131
sathvikavaddepally <sathvikavaddepally@gmail.com>
2025-12-13 12:38:46 +0530

    constructor

--- main.java
+++ main.java
@@ -1,25 +1,36 @@
 class student{
     String name;
+    String location;
+    String hobbies;
     int age;
-    public void display(){
-        System.out.println("name of the student: "+name+" and age: "+age);}}
+public void display(){
+        System.out.println("name of the student: "+name+" , location: "+location+" and hobbies:"+hobbies+" ");}}
         public class main{
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         student s1= new student();
         s1.name= "sathvika";
-        s1.age= 20;
+        s1.location="hanmakonda";
+        s1.hobbies="dancing,listening music";
         s1.display();
 
         student s2= new student();
-        s2.name= "ravichander";
-        s2.age= 42;
+        s2.name= "Medhansh Anirudh";
+        s2.location= "sathupally";
+        s2.hobbies="playing games";
         s2.display();
-         
+        
         student s3= new student();
-        s3.name= "srujana";
-        s3.age=40;
+        s3.name= "Vedhansh";
+        s3.location= "Hyderabad";
+        s3.hobbies="dancing,playing games";
         s3.display();
 
+        student s4= new student();
+        s4.name= "Koushik Vardhan";
+        s4.location= "Hyderabad";
+        s4.hobbies="playing football, e-gaming";
+        s4.display();
+        
     }
     }
     
