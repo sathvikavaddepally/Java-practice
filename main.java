0ee679791f6e443705d0db481f2bdd43596a9801
sathvikavaddepally <sathvikavaddepally@gmail.com>
2025-12-13 12:07:24 +0530

    class and object

---
+++ main.java
@@ -0,0 +1,27 @@
+class student{
+    String name;
+    int age;
+    public void display(){
+        System.out.println("name of the student: "+name+" and age: "+age);}}
+        public class main{
+    public static void main(String[] args) {
+        student s1= new student();
+        s1.name= "sathvika";
+        s1.age= 20;
+        s1.display();
+
+        student s2= new student();
+        s2.name= "ravichander";
+        s2.age= 42;
+        s2.display();
+         
+        student s3= new student();
+        s3.name= "srujana";
+        s3.age=40;
+        s3.display();
+
+    }
+    }
+    
+    
+
