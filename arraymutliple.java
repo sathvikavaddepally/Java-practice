1a827799a794903514977c5327da1689d584dbb2
sathvikavaddepally <sathvikavaddepally@gmail.com>
2025-12-15 13:50:54 +0530

    multiple of 5 with array

--- arraymutliple.java
+++ arraymutliple.java
@@ -2,7 +2,7 @@ public class arraymutliple {
     public static void main(String[]args){
         int[]multiples=new int[100];
     for(int i=1;i<multiples.length;i++){
-        multiples[i-1]=3*i;
+        multiples[i-1]=5*i;
  }
  for(int i=0;i<multiples.length;i++){
 System.out.print(multiples[i]+ " " );
