3fc486ff8f21e41cfaf72ce857967c4ab483751c
sathvikavaddepally <sathvikavaddepally@gmail.com>
2025-12-15 12:28:26 +0530

    array index

---
+++ array.java
@@ -0,0 +1,12 @@
+public class array {
+    public static void main(String[]args){
+        String[] names={"Sathvika","koushik Vardhan","Aqsa","keerthi","suprathika","Hrishikesh","Nihar Adithya","Vedhansh","Medhash Anirudh","Dhanvi"};
+        System.out.println("current first name:");
+        System.out.println(names[5]);
+        System.out.println("Updating first name:");
+        names[5]="New Name 6";
+        System.out.println(names[5]);
+        
+    }
+    
+}
