1e6f1d7ed19bb10349111a9b2cc48431a5aa2ee0
sathvikavaddepally <sathvikavaddepally@gmail.com>
2025-12-15 13:48:34 +0530

    multiple of 2 with array

---
+++ arraymutliple2.java
@@ -0,0 +1,12 @@
+public class arraymutliple2 {
+    public static void main(String[]args){
+        int[]multiples=new int[100];
+    for(int i=0;i<multiples.length;i++){
+        multiples[i]=2*(i+1);
+ }
+ for(int i=0;i<multiples.length;i++){
+System.out.print(multiples[i]+ " " );
+ }
+    }
+    
+}
